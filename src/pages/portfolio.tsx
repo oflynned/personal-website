4ed08b5e721190db5f7b22021bb8d65858535251
Edmond O'Flynn <oflynned@tcd.ie>
2022-03-30 17:06:41 +0200

    chore: refactor resume

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,5 +1,31 @@
 const Portfolio = () => {
-  return <h1>Portfolio</h1>;
+  return (
+    <div>
+      <h1>Portfolio</h1>
+
+      {/*<div className={'mt-4'}>*/}
+      {/*  <Heading>Skills</Heading>*/}
+      {/*  <div className={'mt-4'}>*/}
+      {/*    <ChipHolder>*/}
+      {/*      {showcaseProjects*/}
+      {/*        .reduce((acc: Technology[], project: Project) => {*/}
+      {/*          const technologies = [...acc, ...project.technologies];*/}
+
+      {/*          return [...new Set(technologies)];*/}
+      {/*        }, [])*/}
+      {/*        .sort((a, b) => (a > b ? 1 : -1))*/}
+      {/*        .map((item, index) => {*/}
+      {/*          return (*/}
+      {/*            <div key={index} className={'m-1'}>*/}
+      {/*              <Chip>{item}</Chip>*/}
+      {/*            </div>*/}
+      {/*          );*/}
+      {/*        })}*/}
+      {/*    </ChipHolder>*/}
+      {/*  </div>*/}
+      {/*</div>*/}
+    </div>
+  );
 };
 
 export default Portfolio;
--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,39 +1,45 @@
 import { FC } from 'react';
 import { experience } from '../data/experience';
+import { projects } from '../data/projects';
 import { Heading } from '../design-system/atoms/heading.atom';
 import { Paragraph } from '../design-system/atoms/paragraph.atom';
 import { Page } from '../design-system/components/page';
+import { EducationHistory } from '../design-system/molecules/education-history.molecule';
 import { Header } from '../design-system/molecules/header.molecule';
+import { ProjectHistory } from '../design-system/molecules/project-history.molecule';
 import { WorkHistory } from '../design-system/molecules/work-history.molecule';
 
-const Resume: FC = () => {
-  const {
-    name,
-    summary,
-    email,
-    links,
-    positions,
-    education,
-    languages,
-    awards,
-  } = experience;
+const showcaseProjects = projects.filter((item) => item.resumeWorthy);
+const {
+  name,
+  summary,
+  email,
+  links,
+  positions,
+  qualifications,
+  languages,
+  awards,
+} = experience;
 
+const Resume: FC = () => {
   return (
     <div className={'w-screen min-h-screen flex justify-center'}>
       <Page>
         <Header name={name} summary={summary} email={email} links={links} />
+
         <div className={'mt-8'}>
           <WorkHistory positions={positions} />
         </div>
 
-        <Heading>Projects</Heading>
-        <div className={'mt-2'}>
-          <Paragraph>
-            I have a lot of projects. Head to here for a full overview
-          </Paragraph>
+        <div className={'mt-4'}>
+          <EducationHistory qualifications={qualifications} />
         </div>
 
-        <div className={'mt-8'}>
+        <div className={'mt-4'}>
+          <ProjectHistory projects={showcaseProjects} />
+        </div>
+
+        <div className={'mt-4'}>
           <Heading>Languages</Heading>
           <div className={'mt-2'}>
             <Paragraph>{languages.join(', ')}</Paragraph>
