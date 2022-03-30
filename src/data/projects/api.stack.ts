83b5e640ad63d4fd1637fa4c80aa1b3c8cd3d5eb
Edmond O'Flynn <oflynned@tcd.ie>
2022-03-30 17:28:17 +0200

    chore: move usual api stack to its own list to simplify modern projects

---
+++ src/data/projects/api.stack.ts
@@ -0,0 +1,28 @@
+import {
+  Database,
+  Framework,
+  Language,
+  Platform,
+  Protocol,
+  Technology,
+  Tool,
+} from './showcase';
+
+export const usualApiStack: Technology[] = [
+  Language.TYPESCRIPT,
+  Language.JAVASCRIPT,
+  Framework.NESTJS,
+  Tool.WEBPACK,
+  Platform.API,
+  Protocol.GRAPHQL,
+  Framework.BULLJS,
+  Database.POSTGRESQL,
+  Protocol.REST,
+  Tool.JEST,
+  Framework.SUPERTEST,
+  Framework.NODEJS,
+  Tool.DOCKER,
+  Tool.DOCKER_COMPOSE,
+  Database.REDIS,
+  Protocol.RPC,
+];
