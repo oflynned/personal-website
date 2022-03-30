d30f9a3c5e2aa8b9546a14b6090eac9436ef2e66
Edmond O'Flynn <oflynned@tcd.ie>
2022-03-30 13:07:56 +0200

    chore: fix prop changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 import type { NextPage } from 'next';
+import { Heading } from '../design-system/atoms/heading.atom';
 
 const Home: NextPage = () => {
-  return <div>yo</div>;
+  return (
+    <div
+      className={'flex justify-center items-center w-screen h-screen bg-sky-50'}
+    >
+      <div className={'h-fit justify-self-center'}>
+        <Heading>Hey, I&apos;m Ed</Heading>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
