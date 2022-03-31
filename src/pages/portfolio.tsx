7e4db323780256a668f4324bff928e7ee031a4c6
Edmond O'Flynn <oflynned@tcd.ie>
2022-03-31 14:39:48 +0200

    chore: add navbar to portfolio

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,3 +1,4 @@
+import { NextPage } from 'next';
 import { projects } from '../data/projects';
 import { Project, Technology } from '../data/projects/showcase';
 import { ChipHolder } from '../design-system/atoms/chip-holder.atom';
@@ -5,10 +6,13 @@ import { Chip } from '../design-system/atoms/chip.atom';
 import { Heading } from '../design-system/atoms/heading.atom';
 import { Paragraph } from '../design-system/atoms/paragraph.atom';
 import { Title } from '../design-system/atoms/title.atom';
+import { Navbar } from '../design-system/molecules/nav-bar.molecule';
 
-const Portfolio = () => {
+const Portfolio: NextPage = () => {
   return (
     <div>
+      <Navbar />
+
       <Heading>Portfolio</Heading>
 
       <div className={'mt-4'}>
